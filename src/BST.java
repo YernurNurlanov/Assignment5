f7d1011f4faf62dfd00c0d517723b451525572c0
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-05-20 23:23:06 +0600

    Add delete(K key) and delete(Node node, K key)

--- src/BST.java
+++ src/BST.java
@@ -47,4 +47,41 @@ public class BST<K extends Comparable<K>, V> {
         else
             return node;
     }
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, K key) {
+        if (node == null)
+            return null;
+        int x = key.compareTo(node.key);
+        if (x < 0)
+            node.left = delete(node.left, key);
+        else if (x > 0)
+            node.right = delete(node.right, key);
+        else {
+            if (node.left == null)
+                return node.right;
+            else if (node.right == null)
+                return node.left;
+            else {
+                Node min = findMin(node.right);
+                node.key = min.key;
+                node.val = min.val;
+                node.right = deleteMin(node.right);
+            }
+        }
+        return node;
+    }
+    private Node findMin(Node node) {
+        if (node.left == null)
+            return node;
+        return findMin(node.left);
+    }
+    private Node deleteMin(Node node) {
+        if (node.left == null)
+            return node.right;
+        node.left = deleteMin(node.left);
+        return node;
+    }
 }
